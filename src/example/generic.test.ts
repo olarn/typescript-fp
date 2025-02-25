d9bd939f4263521f563bffd02641ae704545a1b2
Olarn U <olarn.u@odds.team>
2025-02-25 11:21:04 +0700

    Add generic queue number test case.

--- src/example/generic.test.ts
+++ src/example/generic.test.ts
@@ -10,4 +10,14 @@ describe('Generic Queue', () => {
     expect(q.remove()).toEqual('first')
     expect(q.allElements()).toEqual(['second', 'third'])
   })
+
+  it('should work with number queue', () => {
+    const q = new Queue<number>()
+    q.add(1)
+    q.add(2)
+    q.add(3)
+    
+    expect(q.remove()).toEqual(1)
+    expect(q.allElements()).toEqual([2, 3])
+  })
 })
