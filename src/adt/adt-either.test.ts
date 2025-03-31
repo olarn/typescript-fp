7646fb2ce284913291b7bc7a52c242a6a1422b13
Olarn U <olarn.u@odds.team>
2025-03-31 09:33:57 +0700

    Add Either lab files and slides.

--- src/adt/adt-either.test.ts
+++ src/adt/adt-either.test.ts
@@ -1,5 +1,23 @@
+import { Either, match, right, left } from "./adt-either"
+
 describe('Pattern Matching with Either', () => {
-  it('', () => {
-    
+  it('should match right to string', () => {
+    const errorOrNum: Either<string, number> = right(12)
+    expect
+      (match(
+        (e: string) => e,
+        (a: number) => `value is ${a}`
+      )(errorOrNum)
+      ).toEqual(`value is 12`)
+  })
+
+  it('should also match left to string', () => {
+    const errorOrNum: Either<string, number> = left(`error`)
+    expect(
+      match(
+        (e: string) => e,
+        (a: number) => `value is ${a}`
+      )(errorOrNum)
+    ).toEqual(`error`)
   })
 })
--- src/adt/adt-either.ts
+++ src/adt/adt-either.ts
@@ -23,6 +23,12 @@ const right = <A, E = never > (a: A): Either<E, A> => ({
 const isLeft = <E, A>(x: Either<E, A>): x is Left<E> =>
   x._tag === 'left' 
 
-export { left, right, isLeft }
 
 // Pattern Matching ------------------------- 
+
+type Match = <E, A, B>(onLeft: (e: E) => B, onRight: (a: A) => B) => 
+  (x: Either<E, A>) => B
+const match: Match = (onLeft, onRight) => x => 
+  isLeft(x) ? onLeft(x.left) : onRight(x.right)
+
+export { Either, left, right, isLeft, match }
