f8a978e29b00fb70315fc48e47e2cba0805136d9
Olarn U <olarn.u@odds.team>
2025-04-02 17:23:20 +0700

    Add pattern matching lab for list.

---
+++ src/adt/adt-list.test.ts
@@ -0,0 +1,15 @@
+import {
+  // cons, match, nil, showList
+} from './adt-list'
+
+describe('Pattern Matching with Option Type', () => {
+  it('should match list to string', () => {
+    // const list = cons(1, cons(2, cons(3, cons(4, nil))))
+    // expect(
+    //   match(
+    //     () => '',
+    //     (xs) => showList(xs)
+    //   )(list)
+    // ).toEqual('1, 2, 3, 4')
+  })
+})
