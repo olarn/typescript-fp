f2b706836270af5e8f672104b304d5ffd8004781
Olarn U <olarn.u@odds.team>
2025-04-10 20:45:35 +0700

    Updte slide and lab to arity function.

--- src/compose.fp-ts/flow.test.ts
+++ src/compose.fp-ts/flow.test.ts
@@ -1,7 +1,7 @@
 import { flow } from 'fp-ts/function'
 
-type Increment = (x: number) => number
-const increment: Increment = x => x + 1
+type Increment = (x: number, y: number) => number
+const increment: Increment = (x, y) => x + y
 
 type ToString = (x: number) => string
 const toString: ToString = x => `${x}`
@@ -9,6 +9,6 @@ const toString: ToString = x => `${x}`
 describe('pipe', () => {
   it('should increment number then to string with Pipe', () => {
     const result = flow(increment, toString)
-    expect(result(0)).toBe('1')
+    expect(result(2, 2)).toBe('4')
   })
 })
