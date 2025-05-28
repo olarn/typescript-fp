6cb33bf596cf4aa5a186599705f60a0acfdd30e9
Olarn U <olarn.u@odds.team>
2025-05-28 15:32:39 +0700

    Prepare lab files for TypeClass HKT.

--- src/typeclass/typeclass.test.ts
+++ src/typeclass/typeclass.test.ts
@@ -1,32 +1,34 @@
 import { some, none } from "fp-ts/lib/Option"
-import { liftIncrement, liftIncrement2 } from "./typeclass"
 import { left, right } from "fp-ts/lib/Either"
+import {
+  // liftIncrement, liftIncrement2
+} from "./typeclass"
 
 describe('Type class Functor', () => {
 
   const increment = (x: number) => x + 1
   
   it('should lift up then increase option number', () => {
-    expect(
-      liftIncrement(increment)(some(12))
-    ).toEqual(some(13))
+    // expect(
+    //   liftIncrement(increment)(some(12))
+    // ).toEqual(some(13))
   })
 
   it('should get none object', () => {
-    expect(
-      liftIncrement(increment)(none)
-    ).toEqual(none)
+    // expect(
+    //   liftIncrement(increment)(none)
+    // ).toEqual(none)
   })
 
   it('should lift up increase either number', () => {
-    expect(
-      liftIncrement2(increment)(right(12))
-    ).toEqual(right(13))
+    // expect(
+    //   liftIncrement2(increment)(right(12))
+    // ).toEqual(right(13))
   })
 
   it('should get left object', () => {
-    expect(
-      liftIncrement2(increment)(left('error'))
-    ).toEqual(left('error'))
+    // expect(
+    //   liftIncrement2(increment)(left('error'))
+    // ).toEqual(left('error'))
   })
 })
