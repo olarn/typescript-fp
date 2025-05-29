c50fdbdc6c06d08317c8bc891365557a33b0817e
Olarn U <olarn.u@odds.team>
2025-05-29 09:19:24 +0700

    Update slides.

--- src/_playground.test.ts
+++ src/_playground.test.ts
@@ -1,5 +1,14 @@
+import { pipe } from "fp-ts/lib/function"
+
 describe('Playground', () => {
   it('should work', () => {
-    expect(1).toBe(1)
+    expect(
+      pipe(
+        'John',
+        (name) => `Hello, ${name}!`,
+        (greeting) => `${greeting} How are you?`,
+        (message) => `${message} I'm fine, thank you!`
+      )
+    ).toEqual('Hello, John! How are you? I\'m fine, thank you!')
   })
 })
