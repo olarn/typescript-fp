1a6e49727121d398b3a3cbdd279bf403bd3c4f4f
Olarn U <olarn.u@odds.team>
2025-06-09 09:51:15 +0700

    Refactor card approval logic: rename CardApproved to ApprovalResult, update cardType field, and modify requirement.txt for function output structure.

--- src/_lab/cardCriteria.ts
+++ src/_lab/cardCriteria.ts
@@ -4,28 +4,28 @@ export type CardRequest = Readonly<{
   hasEmpCert: boolean
 }>
 
-export type CardApproved = Readonly<{
+export type ApprovalResult = Readonly<{
   name: string
   salary: number
   hasEmpCert: boolean
-  card: string
+  cardType: string
 }>
 
 // ------------------------------------------------------------
 
 export type CardCriteria = Readonly<{
+  cardType: string
   minSalary: number
   maxSalary: number
   requiredEmpCert: boolean
-  card: string
 }>
 
 export const cardCriteria: CardCriteria[] = [
-  { minSalary: 15000, maxSalary: 25000, requiredEmpCert: false, card: 'silver' },
-  { minSalary: 25001, maxSalary: 35000, requiredEmpCert: true, card: 'gold' },
-  { minSalary: 35001, maxSalary: 50000, requiredEmpCert: true, card: 'platinum' },
-  { minSalary: 50001, maxSalary: 100000, requiredEmpCert: true, card: 'diamond' },
-  { minSalary: 100001, maxSalary: 5000000, requiredEmpCert: true, card: 'prestige' },
+  { cardType: 'silver', minSalary: 15000, maxSalary: 25000, requiredEmpCert: false },
+  { cardType: 'gold', minSalary: 25001, maxSalary: 35000, requiredEmpCert: true },
+  { cardType: 'platinum', minSalary: 35001, maxSalary: 50000, requiredEmpCert: true },
+  { cardType: 'diamond', minSalary: 50001, maxSalary: 100000, requiredEmpCert: true },
+  { cardType: 'prestige', minSalary: 100001, maxSalary: 5000000, requiredEmpCert: true },
 ]
 
 export const salaryTooLow = (salary: number): boolean => salary < 15000
